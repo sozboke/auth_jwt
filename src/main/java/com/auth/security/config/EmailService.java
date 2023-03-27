ad2a2ed33312e2329996bd792ba7b5ef19c04f85
beyza ince <beyzaa0204@gmail.com>
2023-03-27 17:28:25 +0300

    mimemessage beyz test

--- src/main/java/com/auth/security/config/EmailService.java
+++ src/main/java/com/auth/security/config/EmailService.java
@@ -1,22 +1,56 @@
 package com.auth.security.config;
 
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import java.io.File;
+import java.util.Properties;
 
 @Service
 public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
+    Properties properties=new Properties();
+    Session session=Session.getDefaultInstance(properties);
 
 
-    public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
+//
+//    @Autowired
+//    private ResourceLoader resourceLoader;
+    public void sendSimpleMessage(String to, String subject, String text) throws MessagingException, jakarta.mail.MessagingException {
+        MimeMessage msg = mailSender.createMimeMessage();
+
+
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(msg, true);
+        mimeMessageHelper.setTo("beyzaa0204@gmail.com");
+        mimeMessageHelper.setText("<h1> checkt attttt</h1>");
+//        Resource file = resourceLoader.getResource("test.jpg");
+//
+//        mimeMessageHelper.addAttachment("test.jpg",file);
+//        mailSender.send(msg);
+
+
+//        Message message = new MimeMessage();
+//        message.setText(text);
+//        message.setFrom(new InternetAddress("beyzaa0204@gmail.com"));
+//        mailSender.send(message);
+
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setC
+//        message.setText("<a href=\"www.google.com\">www.google.com</a>");
+//        mailSender.send(message);
     }
 }
--- src/main/java/com/auth/security/config/RabbitMQReceiver.java
+++ src/main/java/com/auth/security/config/RabbitMQReceiver.java
@@ -7,13 +7,16 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import com.auth.security.config.EmailService;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+
 @RequiredArgsConstructor
 @Service
 public class RabbitMQReceiver {
     private static Logger logger = LogManager.getLogger(RabbitMQReceiver.class.toString());
     private final EmailService emailService;
     @RabbitListener(queues = {"${rabbitmq.json}"})
-    public void receiver(User user) {
+    public void receiver(User user) throws MessagingException, jakarta.mail.MessagingException {
         logger.info("New user registered" + user.toString());
         emailService.sendSimpleMessage(user.getEmail(), "Registration", "Success");
     }
