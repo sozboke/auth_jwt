cda53a17b54ea83fb671324b97726d4c8f340993
servetoz <servetoz@comodo.net>
2023-03-28 14:03:05 +0300

    Mail

--- src/main/java/com/auth/security/config/EmailService.java
+++ src/main/java/com/auth/security/config/EmailService.java
@@ -19,24 +19,21 @@ import java.util.Properties;
 
 @Service
 public class EmailService {
-
     @Autowired
     private JavaMailSender mailSender;
-    Properties properties=new Properties();
-    Session session=Session.getDefaultInstance(properties);
-    //testt
 
-
-//
-//    @Autowired
-//    private ResourceLoader resourceLoader;
     public void sendSimpleMessage(String to, String subject, String text) throws MessagingException, jakarta.mail.MessagingException {
         MimeMessage msg = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(msg, "utf-8");
+        String htmlContent = "<h1>This is a test Spring Boot email</h1>" +
+                "<p>It can contain <strong>HTML</strong> content.</p>";
+        helper.setSubject(subject);
+        helper.setText(htmlContent, true);
+        helper.setTo(to);
+        FileSystemResource file = new FileSystemResource(new File("test.jpg"));
+        helper.addAttachment("test.jpg", file);
 
-
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(msg, true);
-        mimeMessageHelper.setTo("beyzaa0204@gmail.com");
-        mimeMessageHelper.setText("<h1> checkt attttt</h1>");
+        mailSender.send(msg);
 //        Resource file = resourceLoader.getResource("test.jpg");
 //
 //        mimeMessageHelper.addAttachment("test.jpg",file);
